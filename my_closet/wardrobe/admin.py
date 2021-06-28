a41e67410020df1ed2d65b9c6b65fae88fc78d79
davemolk <davemolk@gmail.com>
2021-06-28 14:24:53 -0600

    collapse models into single clothing model

--- my_closet/wardrobe/admin.py
+++ my_closet/wardrobe/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
-from .models import Tag, OutfitTag, User, Top, Bottom, Dress, Shoe, Outerwear, Outfit
+from .models import Tag, OutfitTag, User, Clothing, Outfit
 
 # Register your models here.
-
 
 admin.site.register(Tag)
 admin.site.register(OutfitTag)
 admin.site.register(User)
-admin.site.register(Top)
-admin.site.register(Bottom)
-admin.site.register(Dress)
-admin.site.register(Shoe)
-admin.site.register(Outerwear)
+admin.site.register(Clothing)
 admin.site.register(Outfit)
--- my_closet/wardrobe/models.py
+++ my_closet/wardrobe/models.py
@@ -26,68 +26,22 @@ class User(models.Model):
     def __str__(self):
         return self.full_name()
 
-class Top(models.Model):
+class Clothing(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=300, blank=True, null=True)
-    image = models.ImageField(upload_to="posts", null=True)
+    image = models.ImageField(upload_to="images", null=True)
     tag = models.ManyToManyField(Tag)
     sell = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name()
-
-class Bottom(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=300, blank=True, null=True)
-    image = models.ImageField(upload_to="posts", null=True)
-    tags = models.ManyToManyField(Tag)
-    sell = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name()
-
-
-class Dress(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=300, blank=True, null=True)
-    image = models.ImageField(upload_to="posts", null=True)
-    tags = models.ManyToManyField(Tag)
-    sell = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name()
-
-class Shoe(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=300, blank=True, null=True)
-    image = models.ImageField(upload_to="posts", null=True)
-    tags = models.ManyToManyField(Tag)
-    sell = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name()
-
-class Outerwear(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=300, blank=True, null=True)
-    image = models.ImageField(upload_to="posts", null=True)
-    tags = models.ManyToManyField(Tag)
-    sell = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name()
+        return self.name
 
 class Outfit(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=300, blank=True, null=True)
     outfit_tag = models.ManyToManyField(OutfitTag)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="outfits")
-    top = models.ForeignKey(Top, null=True, on_delete=models.CASCADE, related_name="outfits")
-    bottom = models.ForeignKey(Bottom, null=True, on_delete=models.CASCADE, related_name="outfits")
-    dress = models.ForeignKey(Dress, null=True, on_delete=models.CASCADE, related_name="outfits")
-    shoe = models.ForeignKey(Shoe, null=True, on_delete=models.CASCADE, related_name="outfits")
-    outerwear = models.ForeignKey(Outerwear, null=True, on_delete=models.CASCADE, related_name="outfits")
+    clothing = models.ForeignKey(Clothing, null=True, on_delete=models.CASCADE, related_name="outfits")
 
     def __str__(self):
-        return self.name()
-
+        return self.name
