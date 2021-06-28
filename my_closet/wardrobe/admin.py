510040b66fb75ff924570308a4e2293b9c3ae3f4
davemolk <davemolk@gmail.com>
2021-06-28 13:18:43 -0600

    register models, create superuser

--- my_closet/wardrobe/admin.py
+++ my_closet/wardrobe/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Tag, OutfitTag, User, Top, Bottom, Dress, Shoe, Outerwear, Outfit
 
 # Register your models here.
+
+
+admin.site.register(Tag)
+admin.site.register(OutfitTag)
+admin.site.register(User)
+admin.site.register(Top)
+admin.site.register(Bottom)
+admin.site.register(Dress)
+admin.site.register(Shoe)
+admin.site.register(Outerwear)
+admin.site.register(Outfit)
